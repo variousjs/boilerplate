6861c951ea6763fd8b340355cbb90a7e8a93b904
loeify <loeify@gmail.com>
2021-10-08 08:56:59 +0800

    fix: npm publish

--- bin/copy.js
+++ bin/copy.js
@@ -1,36 +1,28 @@
 const fs = require('fs')
 
-const files = ['.gitignore', 'package-lock.json']
+const files = ['.gitignore', 'package-lock.json', 'package.json']
 const { COPY_ENV } = process.env
 
 files.forEach((name) => {
   const copyName = `${name}.copy`
 
-  if (COPY_ENV === 'publish') {
-    try {
-      fs.unlinkSync(copyName)
-    } catch (e) {
-      // ignore
-    }
-  }
-
   try {
     if (COPY_ENV === 'publish') {
-      fs.copyFileSync(name, copyName, fs.constants.COPYFILE_EXCL)
+      fs.copyFileSync(name, copyName, fs.constants.COPYFILE_FICLONE)
     }
 
     if (COPY_ENV === 'init') {
-      fs.copyFileSync(copyName, name, fs.constants.COPYFILE_EXCL)
+      fs.copyFileSync(copyName, name, fs.constants.COPYFILE_FICLONE)
     }
   } catch (e) {
-    // ignore
+    console.log(e.message)
   }
 
   if (COPY_ENV === 'init') {
     try {
       fs.unlinkSync(copyName)
     } catch (e) {
-      // ignore
+      console.log(e.message)
     }
   }
 })
