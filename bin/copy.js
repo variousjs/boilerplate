7f27f3d431d769fb60c528c6afea09dd3ff3c5ad
loeify <loeify@gmail.com>
2021-10-08 23:37:01 +0800

    fix: npm script

--- bin/copy.js
+++ bin/copy.js
@@ -1,13 +1,33 @@
 const fs = require('fs')
+const package = require('../package.json')
 
 const files = ['.gitignore', 'package-lock.json', 'package.json']
 const { COPY_ENV } = process.env
+
+if (COPY_ENV === 'publish') {
+  package.name = 'variousjs'
+  package.version = '0.1.0'
+  package.private = true
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const { prepublishOnly, postinstall, ...rest } = package.scripts
+  package.scripts = rest
+
+  delete package.bin
+  delete package.publishConfig
+  delete package.repository
+  delete package.keywords
+  delete package.bugs
+  delete package.homepage
+
+  fs.writeFileSync('package.json.copy', JSON.stringify(package, null, 2))
+}
 
 files.forEach((name) => {
   const copyName = `${name}.copy`
 
   try {
-    if (COPY_ENV === 'publish') {
+    if (COPY_ENV === 'publish' && name !== 'package.json') {
       fs.copyFileSync(name, copyName, fs.constants.COPYFILE_FICLONE)
     }
 
