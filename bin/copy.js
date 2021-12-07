a6062a3b13f0a8746a46e91a61de789ef8f12e07
loeify <loeify@gmail.com>
2021-12-07 08:31:53 +0800

    fix: package-lock config

--- bin/copy.js
+++ bin/copy.js
@@ -1,11 +1,15 @@
 const fs = require('fs')
 const package = require('../package.json')
+const packageLock = require('../package-lock.json')
 
 const files = ['.gitignore', 'package-lock.json', 'package.json']
 
 package.name = 'variousjs'
 package.version = '0.1.0'
 package.private = true
+
+packageLock.name = 'variousjs'
+packageLock.version = '0.1.0'
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const { prepublishOnly, ...rest } = package.scripts
@@ -19,12 +23,13 @@ delete package.bugs
 delete package.homepage
 
 fs.writeFileSync('package.json.copy', JSON.stringify(package, null, 2))
+fs.writeFileSync('package-lock.json.copy', JSON.stringify(packageLock, null, 2))
 
 files.forEach((name) => {
   const copyName = `${name}.copy`
 
   try {
-    if (name !== 'package.json') {
+    if (name !== 'package.json' && name !== 'package-lock.json') {
       fs.copyFileSync(name, copyName, fs.constants.COPYFILE_FICLONE)
     }
   } catch (e) {
